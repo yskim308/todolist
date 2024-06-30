63405f787e58d4b1ff4252a252566ce32a2c1e65
yskim308 <youngseokim7@gmail.com>
2024-06-30 22:01:56 +0900

    make description for modal change height dynamically

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import './style.css';
 //event listeners for modal and modal input 
 const inputButton = document.querySelector("#taskInputButton");
 inputButton.addEventListener('click', ()=>{
+    const textArea = document.querySelector("#taskDescription");
+    textArea.addEventListener("keyup", ()=>{
+        textArea.style.height = "10px";
+        textArea.style.height = textArea.scrollHeight + 'px';
+    })
     const dialog = document.querySelector("#taskDialog"); 
     dialog.showModal(); 
 })
