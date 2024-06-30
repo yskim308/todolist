eeaaa9e96d751da9e7445162b1c461750bc19bc4
yskim308 <youngseokim7@gmail.com>
2024-06-30 22:17:47 +0900

    implement logic for closing modal when clicking cancel or outside field

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import './style.css';
 
-//event listeners for modal and modal input 
+//event listener for button to add tasks
 const inputButton = document.querySelector("#taskInputButton");
 inputButton.addEventListener('click', ()=>{
+    //dynmacailly resize description box 
     const textArea = document.querySelector("#taskDescription");
     textArea.addEventListener("keyup", ()=>{
         textArea.style.height = "10px";
         textArea.style.height = textArea.scrollHeight + 'px';
     })
+    //show modal 
     const dialog = document.querySelector("#taskDialog"); 
     dialog.showModal(); 
+    //close modal when clicking on cancel or clicking outside of form
+    const cancel = document.querySelector("#taskCancel");
+    cancel.addEventListener("click", ()=>{
+        dialog.close();
+    })
+    const formContainer = document.querySelector("#formContainer");
+    formContainer.addEventListener('click', (event)=>{event.stopPropagation()}) 
+    dialog.addEventListener('click', ()=>{dialog.close();})
 })
+//modal submit and cancel event listener
