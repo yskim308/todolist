ec91a1c9fa69ca7af181052e75c413389d0a394c
yskim308 <youngseokim7@gmail.com>
2024-07-01 22:20:40 +0900

    implement logic to add tasks to taskList object

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 import './style.css';
+import {createTask, createTaskList, createTasklist} from './taskClasses.js';
 
+const dialog = document.querySelector("#taskDialog"); 
 //event listener for button to add tasks
 const inputButton = document.querySelector("#taskInputButton");
 inputButton.addEventListener('click', ()=>{
-    //dynmacailly resize description box 
-    const textArea = document.querySelector("#taskDescription");
-    textArea.addEventListener("keyup", ()=>{
-        textArea.style.height = "10px";
-        textArea.style.height = textArea.scrollHeight + 'px';
-    })
     //show modal 
-    const dialog = document.querySelector("#taskDialog"); 
     dialog.showModal(); 
     //close modal when clicking on cancel or clicking outside of form
     const cancel = document.querySelector("#taskCancel");
@@ -20,4 +15,29 @@ inputButton.addEventListener('click', ()=>{
     const formContainer = document.querySelector("#formContainer");
     formContainer.addEventListener('click', (event)=>{event.stopPropagation()}) 
     dialog.addEventListener('click', ()=>{dialog.close();})
+});
+
+//dynmacailly resize description box 
+const textArea = document.querySelector("#taskDescription");
+textArea.addEventListener("keyup", ()=>{
+    textArea.style.height = "10px";
+    textArea.style.height = textArea.scrollHeight + 'px';
+});
+
+//logic for adding tasks to taskList object
+//event listener for modal form submission 
+let taskList = createTaskList();
+const modalForm = document.querySelector("#taskInput");
+modalForm.addEventListener('submit', (event)=>{
+    event.preventDefault(); 
+    let taskName = document.querySelector("#taskName").value;
+    let taskDescription = document.querySelector("#taskDescription").value; 
+    let dueDate = document.querySelector("#taskDate").value; 
+    //create task and insert into taskList object
+    let task = createTask(taskName, taskDescription, dueDate); 
+    taskList.addTask(task);
+
+    console.log(taskList.todo)
+    event.target.reset();
+    dialog.close();
 })
--- src/taskClasses.js
+++ src/taskClasses.js
@@ -1,9 +1,9 @@
-function task(name, description, dueDate){
-    finished = false
+function createTask(name, description, dueDate){
+    let finished = false;
     return {name: name, description: description, dueDate: dueDate, finished};
 }
 
-function taskList(){
+function createTaskList(){
     let todo = [];
     function addTask(object){
         let length = todo.length;
@@ -14,19 +14,23 @@ function taskList(){
             }
         }
         todo.push(object);
-        return;
     } 
     
     function removeTask(object){
         let length = todo.length;
-        if (length) {
+        if (!length) {
             console.log(object + " cannot be removed, list is empty!");
-            return 
+            return;
         }
-        todo.length
+        for (let i = 0; i < length; i++){
+            if (todo[i].name === object.name){
+                todo.splice(i, 1);
+                return;
+            }
+        }
     }
 
     return {todo, addTask, removeTask}
 }
 
-export {task, taskList};
+export {createTask, createTaskList};
