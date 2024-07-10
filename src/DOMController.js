8df8ac76b527080202dde00f1a98485c72159a13
Young Seo <youngseokim7@gmail.com>
2024-07-10 17:56:31 +0900

    refactored DOMcontroller such that each component created is its own function

--- src/DOMController.js
+++ src/DOMController.js
@@ -8,29 +8,50 @@ function createDOMController(objectArray){
         }
     }
 
-    function displayTask(task){
-        const taskContainer = document.createElement('div'); 
-        taskContainer.classList.add('flex', 'p-3');
-
+    function createCheck(){ 
         const checkBox = document.createElement('input'); 
         checkBox.type = 'checkbox'; 
         checkBox.id = 'taskFinish'; 
         checkBox.name = 'task'; 
+        return checkBox; 
+    } 
+
+    
+    function createHeader(taskName){
+        const header = document.createElement('contaiener'); 
+        header.innerText = taskName; 
+        return header; 
+    }
+
+    function createDescription(taskDescription){
+        const description = document.createElement('container');
+        description.innerHTML = taskDescription;
+        return description;
+
+    }
+
+    function createDate(dueDate){
+        const taskDueDate = document.createElement('div');
+        taskDueDate.innerHTML = dueDate; 
+        return taskDueDate;
+    }
+
+    function displayTask(task){
+        const taskContainer = document.createElement('div'); 
+        taskContainer.classList.add('flex', 'p-3');
+        const checkBox = createCheck();
         taskContainer.appendChild(checkBox);
 
-        const taskContent = document.createElement('container');    
-        const taskHeader = document.createElement('div');
-        taskHeader.innerText = task.name; 
-        const taskDescription = document.createElement('div'); 
-        taskDescription.innerText = task.description; 
-        taskContent.appendChild(taskHeader); 
-        taskContent.appendChild(taskDescription);
+        const contentContainer = document.createElement('container');    
+        const taskHeader = createHeader(task.name);
+        const taskDescription = createDescription(task.description);
+        contentContainer.appendChild(taskHeader);
+        contentContainer.appendChild(taskDescription);
         if (task.dueDate){
-            const taskDueDate = document.createElement('div'); 
-            taskDueDate.innerText = task.dueDate
-            taskContent.appendChild(taskDueDate); 
+            const taskDueDate = createDate(task.dueDate);
+            contentContainer.appendChild(taskDueDate); 
         }
-        taskContainer.appendChild(taskContent);
+        taskContainer.appendChild(contentContainer);
 
         document.querySelector("#listContainer").appendChild(taskContainer);
     }
